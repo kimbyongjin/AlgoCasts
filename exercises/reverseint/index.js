b77e1af922f434f4ab98162077002f3677f3d212
Daniel Kim <nautilus@Nautilus.local>
2019-09-25 12:30:39 -0700

    Add initial notes and considerations ioce for reverseInteger

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -11,3 +11,26 @@
 function reverseInt(n) {}
 
 module.exports = reverseInt;
+
+/*
+Take in a number and return a new number that is the representation of the first number
+  but with the digits reversed.
+  This number should remain negative if the input was negative.
+  Also, there should be no leading zeroes.
+  i.e. 500 => 5
+
+i - number (integer for the sake of this example)
+o - number with the digits reversed
+c - This number should remain negative if the input was negative.
+  Also, there should be no leading zeroes.
+  i.e. 500 => 5
+e - negative numbers, trailing / leading zeroes, non-integer numbers,
+
+Consider using the toString() method of converting the number to a string,
+  performing the reversal on the number,
+  convert string number back to a datatype number
+  handle what was trailing zeroes that are now leading zeroes
+  handle negative input that must now also be negative output
+  return reversed number
+
+*/
