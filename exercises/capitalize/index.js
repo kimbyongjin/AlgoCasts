8deef6be97761fcd1a09465d58f6eee80b3e9d5d
Daniel Kim <nautilus@Nautilus.local>
2019-09-30 09:29:06 -0700

    Add iniitial notes and planning to prompt

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -10,3 +10,27 @@
 function capitalize(str) {}
 
 module.exports = capitalize;
+
+/*
+write a function that takes in a string expression and returns that same string expression but with
+  the first character of every word in that string capitalized.
+
+i - string expression
+o - same string expression but with the first character of every word in that string expression capitalized
+c - n/a
+e - string expression that has other capital letters in it. I can leave them the same.
+  But, I must follow the logic that the first char of each word in the string is always capital
+
+One solution would be to iterate through the string. And, find every character that === ' '.
+Every space character must be followed by a capitalized string character. I could, in one fell swoop,
+  iterate through and follow each space character with a .toUpperCase() method call.
+
+  This is a pretty brute force solution. But, It's also simple and concise.
+
+How can I build in a helper function to separate the operations of this function?
+
+I will write a function that will identify a space character, then call the helper function
+  on the character immediately after the space character.
+
+Although, itmght be just as easy to write it out in a single step.
+*/
