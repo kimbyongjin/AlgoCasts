2c52156c8afbab5cf2f7e02f55d23a880a2044fe
Daniel Kim <nautilus@Nautilus.local>
2019-10-01 13:09:39 -0700

    Submit alternative RegExp solution and additional notes about using RegExp methods

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,17 +7,46 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {
-  let vowels = ['a', 'e', 'i', 'o', 'u'];
-  let counter = 0;
+// Basic, naive iterative solution using a hacky array of vowels
+// function vowels(str) {
+//   let vowelCheck = ['a', 'e', 'i', 'o', 'u'];
+//   let counter = 0;
 
-  for (let char of str) {
-    if (vowels.includes(char.toLowerCase())) {
-      counter++;
-    }
-  }
-  return counter;
+//   for (let char of str.toLowerCase()) {
+//     if (vowelCheck.includes(char) {
+//       counter++;
+//     }
+//   }
+//   return counter;
+// };
+
+
+// SLick, concise solution using a regexp to replace all of the non-vowels with an empty string, ignore case,
+// and return the length fo all remaining characters that don't match the pattern.
+// function vowels(str) {
+//   return str.replace(/[^aeiou]/gi, '').length;
+// };
+
+// Alternative regexp solution usinig string.match
+function vowels(str) {
+  // in this example, matches returns an array of all matches to the regexp, or it returns
+    // null if no matches found. So the ternary expression handles a falsey value of null, or the length of
+    // the array of matches which will all be vowels.
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
 };
+
+// RegExp pattern matches:
+  // ^ -> In this case as the first character in a character set, the carrot denotes a negated
+    // or complemented character set. This pattern will match everything NOT included in the brackets.
+
+  // the vowels a*e*i*o*u select for those letter specifically, not a range.
+
+  // the g flag after the closing regexp bracket and slash denotes a global search, checking the
+    // entire string expression
+
+  // the i flag after the closing regexp bracket and slash denotes a case incensitive search. This
+    // is important so that the function counts all vowel occurances regardless of case.
 
 module.exports = vowels;
 
