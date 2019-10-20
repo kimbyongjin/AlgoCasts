9f8eba1b75ccc983f8d1148829930b302ee8511a
Daniel Kim <nautilus@Nautilus.local>
2019-10-20 12:49:50 -0700

    Submit full implementation for LiknedList.js

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -34,10 +34,14 @@ class LinkedList {
 
   getLast() {
     let currentNode = this.head;
-    while (currentNode.next) {
-      currentNode = currentNode.next
+    if (currentNode) {
+      while (currentNode.next) {
+        currentNode = currentNode.next;
+      }
+      return currentNode;
+    } else {
+      return null;
     }
-    return currentNode;
   }
 
   clear() {
@@ -70,6 +74,69 @@ class LinkedList {
       last.next = new Node(data);
     } else {
       this.head = new Node(data);
+    }
+  }
+
+
+  getAt(index) { // returns the node at the provided index
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (this.head) {
+      if (index === 0) { // deletes the first node
+        this.head = this.head.next;
+      } else {
+        const previous = this.getAt(index - 1); // node is the previous-to-be-deleted node
+        if (previous) {
+          if (previous.next && previous.next.next) {
+            previous.next = previous.next.next;
+            return;
+          } else {
+            previous.next = null;
+          }
+        }
+      }
+    }
+  }
+
+  insertAt(data, index) {
+    if (index === 0 ) {
+      this.insertFirst(data, this.head);
+    } else {
+      const previous = this.getAt(index - 1);
+      if (previous) {
+        previous.next = new Node(data, previous.next);
+      } else {
+        this.insertLast(data);
+      }
+    }
+  }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+    while (node) {
+      fn(node, counter);
+      node = node.next
+      counter++;
+    }
+  }
+
+  *[Symbol.iterator]() {
+    let node = this.head;
+    while (node) {
+      yield node;
+      node = node.next
     }
   }
 }
